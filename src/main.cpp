85eaa988e519f0d93d1f672bba24a8bc988b1c8c
Vincent Dupont <vidupont@gmail.com>
2017-09-16 12:40:39 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @Author: Vincent Dupont <vincent>
+ * @Date:   2017-09-16T11:00:12+02:00
+ * @Email:  vidupont@gmail.com
+ * @Filename: main.cpp
+ * @Last modified by:   vincent
+ * @Last modified time: 2017-09-16T12:16:17+02:00
+ */
+
+
+
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
